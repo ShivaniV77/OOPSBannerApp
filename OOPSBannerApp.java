ed753c666ba925ad848a65bdbb119dbe74c52fb4
Shivani Vipin <shivani@Shivanis-MacBook-Air.local>
2026-03-11 11:05:40 +0530

    Added UC6 static functions

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -2,18 +2,60 @@ public class OOPSBannerApp {
 
     public static void main(String[] args) {
 
-        String[] banner = {
-            String.join("", " ***** ", "  ", " ***** ", "  ", " ***** ", "  ", " ***** "),
-            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "),
-            String.join("", "*     *", "  ", "*     *", "  ", "*     *", "  ", "*      "),
-            String.join("", "*     *", "  ", "*     *", "  ", " ***** ", "  ", " ***** "),
-            String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "      *"),
-            String.join("", "*     *", "  ", "*     *", "  ", "*      ", "  ", "      *"),
-            String.join("", " ***** ", "  ", " ***** ", "  ", "*      ", "  ", " *****")
+        String[][] letters = {
+                createO(),
+                createO(),
+                createP(),
+                createS()
         };
 
-        for (String line : banner) {
+        for (int i = 0; i < 7; i++) {
+            String line = "";
+
+            for (String[] letter : letters) {
+                line += letter[i] + "  ";
+            }
+
             System.out.println(line);
         }
+    }
+
+    // Method to create O
+    public static String[] createO() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                "*     *",
+                " ***** "
+        };
+    }
+
+    // Method to create P
+    public static String[] createP() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*     *",
+                " ***** ",
+                "*      ",
+                "*      ",
+                "*      "
+        };
+    }
+
+    // Method to create S
+    public static String[] createS() {
+        return new String[]{
+                " ***** ",
+                "*     *",
+                "*      ",
+                " ***** ",
+                "      *",
+                "*     *",
+                " ***** "
+        };
     }
 }
